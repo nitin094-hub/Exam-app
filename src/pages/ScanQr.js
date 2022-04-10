40d0273dd3353ceb2af54685bef5834d82fb1991
Nitin Rajesh <nitinrajesh094@gmail.com>
2022-04-10 15:58:59 +0530

    Qr Code Scanner

--- src/pages/ScanQr.js
+++ src/pages/ScanQr.js
@@ -2,13 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import { QrReader } from "react-qr-reader";
 
 function ScanQr() {
-  const [scannedResult, setScannerResult] = useState(null);
+  const [scannedResult,setScannerResult] = useState(null);
 
   return (
     <>
       <div style={{ width: "10rem", height: "10rem" }}>
         {!scannedResult && (
           <QrReader
+          facingMode={"rear"}
             onResult={(result, error) => {
               if (!!result) {
                 setScannerResult(result?.text);
@@ -17,7 +18,10 @@ function ScanQr() {
           />
         )}
       </div>
-      <h1>{scannedResult}</h1>
+      {
+        <h1>{scannedResult}</h1>
+      }
+      <button>Submit</button>
     </>
   );
 }
